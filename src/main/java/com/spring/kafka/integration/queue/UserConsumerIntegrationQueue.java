ba6c34a34738872e1560c09e5e05126197c2a393
cbyk-dev <robsongrillo13@gmail.com>
2020-04-13 12:24:29 -0300

    feat(devetools): include devtools

--- src/main/java/com/spring/kafka/integration/queue/UserConsumerIntegrationQueue.java
+++ src/main/java/com/spring/kafka/integration/queue/UserConsumerIntegrationQueue.java
@@ -3,7 +3,6 @@ package com.spring.kafka.integration.queue;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import com.spring.kafka.domain.User;
 
 @Component
 public class UserConsumerIntegrationQueue {
